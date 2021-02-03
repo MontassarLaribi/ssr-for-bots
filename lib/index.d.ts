71a9165d588dfd9d3e46f88786c4a7defa3f4f89
Montassar Laribi <mohamedmontassar.laribi@esprit.tn>
2021-02-03 12:38:27 +0100

    Update default values 1.0.1c

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 interface Options {
-    prerender: Array<string>;
-    exclude: Array<string>;
-    useCache: boolean;
-    cacheRefreshRate: number;
+    prerender?: Array<string>;
+    exclude?: Array<string>;
+    useCache?: boolean;
+    cacheRefreshRate?: number;
 }
-declare function ssrForBots(options: Options): (req: Request, res: Response, next: NextFunction) => void;
+declare function ssrForBots(options?: Options): (req: Request, res: Response, next: NextFunction) => void;
 export default ssrForBots;
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,21 @@ import { NextFunction, Request, Response } from "express";
 import { ssr } from "./ssr";
 
 interface Options {
-  prerender: Array<string>;
-  exclude: Array<string>;
-  useCache: boolean;
-  cacheRefreshRate: number;
+  prerender?: Array<string>;
+  exclude?: Array<string>;
+  useCache?: boolean;
+  cacheRefreshRate?: number;
 }
 
-function ssrForBots(options: Options) {
-  options = Object.assign(
+function ssrForBots(
+  options: Options = {
+    prerender: [], // Array containing the user-agents that will trigger the ssr service
+    exclude: [], // Array containing paths and/or extentions that will be excluded from being prerendered by the ssr service
+    useCache: true, // Variable that determins if we will use page caching or not
+    cacheRefreshRate: 86400, // Seconds of which the cache will be kept alive, pass 0 or negative value for infinite lifespan
+  }
+) {
+  let applyOptions = Object.assign(
     {
       prerender: [], // Array containing the user-agents that will trigger the ssr service
       exclude: [], // Array containing paths and/or extentions that will be excluded from being prerendered by the ssr service
@@ -56,19 +63,19 @@ function ssrForBots(options: Options) {
         const userAgent: string = req.headers["user-agent"] || "";
 
         const prerender = new RegExp(
-          [...prerenderArray, ...options?.prerender].join("|").slice(0, -1),
+          [...prerenderArray, ...applyOptions.prerender].join("|").slice(0, -1),
           "i"
         ).test(userAgent);
 
         const exclude = !new RegExp(
-          [...excludeArray, ...options?.exclude].join("|").slice(0, -1)
+          [...excludeArray, ...applyOptions.exclude].join("|").slice(0, -1)
         ).test(req.originalUrl);
 
         if (req.originalUrl && prerender && exclude) {
           const { html, status } = await ssr(
             req.protocol + "://" + req.get("host") + req.originalUrl,
-            options?.useCache,
-            options?.cacheRefreshRate
+            applyOptions.useCache,
+            applyOptions.cacheRefreshRate
           );
           return res.status(status).send(html);
         } else {
